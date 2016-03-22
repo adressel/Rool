d1182da685827e1fbf107765d5cfbb5fe12c32f5
Andreas Dressel <adress@umich.edu>
2016-03-22 14:08:52 -0400

    renamed Node (PR & HITS) properly

--- src/main/java/andreasdressel/server/util/Node.java
+++ src/main/java/andreasdressel/server/util/Node.java
@@ -1,29 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package andreasdressel.server.util;
 
 /**
  *
  * @author Andreas Dressel
  */
-public class QueryHit {
+public abstract class Node {
   
   private final int id;
-  private final double score;
   
-  public QueryHit(int id, double score) {
+  public Node(int id) {
     this.id = id;
-    this.score = score;
   }
   
   public int getID() {
     return this.id;
   }
   
-  public double getScore() {
-    return this.score;
-  }
 }
